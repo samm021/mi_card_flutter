00d439f243bc1ee04fd90a9850f9414534102a63
samm021 <mohammed.ismail.pribadi@gmail.com>
2021-03-04 18:19:58 +0700

    mi card first challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.redAccent
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.yellowAccent
+                  ),
+                  Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.greenAccent
+                  ),
+                ],
+              ),
+              Container(
+                  width: 100.0,
+                  color: Colors.blueAccent
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
+
